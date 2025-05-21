52679e21c57744ea86f2d65cd8b7a069d6a7a51b
gcalvo5 <gabiolimpic@gmail.com>
2025-05-21 11:35:45 +0200

    Project Created

---
+++ src/main/kotlin/com/planes/PlanesBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.planes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlanesBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<PlanesBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/planes/PlanesBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.planes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlanesBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
